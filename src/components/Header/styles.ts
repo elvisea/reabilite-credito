0522537db02e36d49e3732e9c753d642dbc18bf7
Elvis <elvis.e.amancio@gmail.com>
2022-01-09 22:53:12 -0300

    first commit

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  height: 80px;
+  width: 100%;
+  max-width: 1920px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 150px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    a {
+      margin-right: 20px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+
+      &:hover {
+        color: #FC7900;
+      }
+    }
+  }
+
+  #redes-sociais {
+    width: 100%;
+    max-width: 86px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    a {
+     
+      svg {
+        color: #00AEEF;
+        font-size: 18px;
+
+        &:hover {
+          color: #FC7900;
+        }
+      }
+    }
+  }
+
+  button {
+    height: 40px;
+    max-width: 220px;
+    width: 100%;
+    border: 2px solid #2E3192;
+    font-size: 14px;
+    font-weight: 700;
+    border-radius: 20px;
+    background: #fff;
+    color: #2E3192;
+    
+    &:hover {
+      color: #FC7900;
+      border: 2px solid #FC7900;
+    }
+  }
+
+
+  @media (max-width: 1010px){
+    flex-direction: column;
+  }
+`;
